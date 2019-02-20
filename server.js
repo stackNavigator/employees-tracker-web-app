654aa396cf8b3e51d7809d10bc477c4bd07bbbb4
Alex <zaychenko96y@outlook.com>
2019-02-20 20:25:25 +0200

    Added async function for main process in express

--- server.js
+++ server.js
@@ -5,19 +5,26 @@ const bodyParser = require('body-parser')
 require('dotenv').config()
 const { PORT } = process.env
 
-express()
-  .use(cors())
-  .disable('x-powered-by')
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true}))
-  .get('/', (_, res) => res.status(200).json({ message: 'nice!'}))
-  .use((_, res, __) => {
-    return res.status(404).json({
-      message: 'Resource was not found.'
-    })
-  })
-  .listen(PORT, err => {
-    if (!err) {
-      console.log(`Server successfully runs on port ${PORT}\n`)
-    }
-  })
+;(async () => {
+  try {
+    express()
+      .use(cors())
+      .disable('x-powered-by')
+      .use(bodyParser.json())
+      .use(bodyParser.urlencoded({ extended: true }))
+      .get('/', (_, res) => res.status(200).json({ message: 'nice!' }))
+      .use((_, res, __) => {
+        return res.status(404).json({
+          message: 'Resource was not found.'
+        })
+      })
+      .listen(PORT, err => {
+        if (!err) {
+          console.log(`Server successfully runs on port ${PORT}\n`)
+        }
+      })
+  }
+  catch (err) {
+    console.log(err)
+  }
+})()
