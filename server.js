bdff214a9622e75c25d587a20a82da393ed8cadd
Alex <zaychenko96y@outlook.com>
2019-02-20 20:52:48 +0200

    Added test case for get request

--- server.js
+++ server.js
@@ -8,14 +8,15 @@ const { connectToDb } = require('./services/init-db')
 ;(async () => {
   try {
     const { PORT, DB_URI, DB_NAME } = process.env
-    const db = connectToDb(DB_URI, DB_NAME)
-
+    const db = await connectToDb(DB_URI, DB_NAME)
+    const rofl = await db.collection('employees').countDocuments()
+    
     express()
       .use(cors())
       .disable('x-powered-by')
       .use(bodyParser.json())
       .use(bodyParser.urlencoded({ extended: true }))
-      .get('/', (_, res) => res.status(200).json({ db }))
+      .get('/', (_, res) => res.status(200).json({ rofl }))
       .use((_, res, __) => {
         return res.status(404).json({
           message: 'Resource was not found.'
