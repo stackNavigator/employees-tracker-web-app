ea6b72ab2d168ac86ba43457333501436dcf7a75
Alex <zaychenko96y@outlook.com>
2019-06-25 01:40:21 +0300

    Added validation and basic setup for sign in component

--- frontend/components/SignIn.js
+++ frontend/components/SignIn.js
@@ -1,53 +1,99 @@
 import React, { Component } from 'react'
+import { validate } from '@babel/types';
 
 export class SignIn extends Component {
   constructor() {
     super()
     this.state = {
       text: 'Вхід до системи',
-      username: '',
-      password: ''
+      inputs: [
+        {
+          name: 'username',
+          value: '',
+          min: 2,
+          max: 20,
+          pattern: '^[a-zA-Z0-9]+_?[a-zA-Z0-9]+$',
+          patternMsg: 'Дозволяються лише англійські літери, цифри та символ "_".',
+          placeholder: 'Введіть логін'
+        },
+        {
+          name: 'password',
+          value: '',
+          min: 8,
+          max: 64,
+          pattern: '(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*',
+          patternMsg: 'Необхідна щонайменше одна велика літера і цифра (англійські літери).',
+          placeholder: 'Введіть пароль'
+        }
+      ]
     }
   }
 
-  handleInputChange = ({ target: { name, value}}) => {
+  handleInputChange = ({ target: { name, value } }) => {
+    this.setState(prevState => ({
+      inputs: prevState.inputs.map(input => (
+        input.name === name
+          ? { ...input, value }
+          : input
+      ))
+    }))
+  }
 
+  validateInput = input => {
+    const { name } = input
+    const inputDOM = document.querySelector(`[name=${name}]`)
+    const { validity } = inputDOM
+    switch (true) {
+      case validity.tooShort:
+        const { min } = input
+        inputDOM.setCustomValidity(`Мінімальна кількість символів: ${min}.`)
+        return false
+      case validity.valueMissing:
+        inputDOM.setCustomValidity('Заповніть це поле.')
+        return false
+      case validity.patternMismatch:
+        const { patternMsg } = input
+        inputDOM.setCustomValidity(patternMsg)
+        return false
+      default:
+        inputDOM.setCustomValidity('')
+        return true
+    }
+  }
+
+  handleSubmit = () => {
+    const validated = this.state.inputs.map(input => this.validateInput(input))
+    if (!validated.includes(false))
+      console.log('submitted')
+    
   }
 
   render() {
+    const inputs = this.state.inputs.map(input => {
+      const { name, value, min, max, pattern, placeholder } = input
+      return (
+        <div className="col s12" key={name}>
+          <div className="col s12 input-field" key={name}>
+            <input key={name} name={name} onChange={this.handleInputChange}
+            value={value} onInput={() => this.validateInput(input)}
+            type="text" placeholder={placeholder} minLength={min}
+            maxLength={max} pattern={pattern} required />
+          </div>
+        </div>
+      )
+    })
 
     return (
       <div>
         <h3 className="center-align">{this.state.text}</h3>
-        <form className="row">
-          <div className="col s12">
-            <div className="col s12 input-field">
-              <input
-              name="login"
-              onChange={this.handleInputChange}
-              value={this.state.login}
-              onKeyDown={this.handleKeyDown}
-              type="text"
-              placeholder="Введіть логін"
-              min="2"
-              maxLength="20"
-              required />
-            </div>
-          </div>
-          <div className="col s12">
-            <div className="col s12 input-field">
-              <input
-              name="password"
-              onChange={this.handleInputChange}
-              value={this.state.password}
-              onKeyDown={this.handleKeyDown}
-              type="text"
-              placeholder="Введіть пароль"
-              min="2"
-              maxLength="20"
-              required />
-            </div>
-          </div>
+        <form className="row" onSubmit={e => e.preventDefault()}>
+          {inputs}
+          <div className="col s12 center-align">
+          <button className="btn-flat" onClick={this.handleSubmit}>
+            Увійти до системи
+            <i className="material-icons right">assignment</i>
+          </button>
+        </div>
         </form>
       </div>
     )
