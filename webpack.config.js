66413d94b3603b239232c4c60b440d74de674001
Alex <zaychenko96y@outlook.com>
2019-07-19 23:12:24 +0300

    Added material icons local dependency

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,12 @@ module.exports = {
       {
         test: /\.css$/,
         use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.ttf$/,
+        use: {
+          loader: 'url-loader'
+        }
       }
     ]
   }
